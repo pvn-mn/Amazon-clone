5a0152a14e0d48fda6d40f703d642019a6149e63
pvn-mn <prenavan.m@gmail.com>
2025-06-05 15:25:35 +0530

    home cart
    made the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -80,8 +80,18 @@ products.forEach((product) => {
                 quantity: 1
             });
             }
+
+            let cartQuantity = 0;
+
+            cart.forEach((item) => {
+                cartQuantity += item.quantity
+            })
             
-            console.log(cart);
+
+            document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
+
+            // console.log(cartQuantity)
+            // console.log(cart);
         
         });
     });
