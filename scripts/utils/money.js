b5dba21a4af06360c327236459f3c5ac9452fdee
pvn-mn <prenavan.m@gmail.com>
2025-06-06 16:44:30 +0530

    save data and generate HTML for delivery options

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,6 @@
 export function formatCurrency(priceCents) {
    return (priceCents / 100).toFixed(2);
 }
+
+export default formatCurrency; //defualt export is for importing the function without the '{}'
+//each file can have only one default export
