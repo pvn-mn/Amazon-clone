3225c35133c7a63185377bff7d2fe9c3d98cf619
pvn-mn <prenavan.m@gmail.com>
2025-06-06 09:00:48 +0530

    Delete cart products
    checkout js delete btn

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -43,7 +43,7 @@ cartSummaryHTML += `
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -97,5 +97,17 @@ cartSummaryHTML += `
           </div>
           `;
 });
+
+
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
 // console.log(cartSummaryHTML);
+
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+    link.addEventListener('click', () => {
+        const productId = link.dataset.productId;
+        // console.log(productId);
+        removeFromCart(productId);
+        // console.log(cart);
+    });
+});
