b3c0ec924c66eae5f45dbf55fda2fbe99bee5ed3
pvn-mn <prenavan.m@gmail.com>
2025-06-06 09:10:01 +0530

    delete products from HTML
    checkout js delete btn

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -20,7 +20,7 @@ cart.forEach((cartItem) => {
 
 
 cartSummaryHTML += `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -109,5 +109,9 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
         // console.log(productId);
         removeFromCart(productId);
         // console.log(cart);
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        // console.log(container);
+        container.remove();
     });
 });
