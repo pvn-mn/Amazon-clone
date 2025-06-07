0593e031dc68f843ead2d94838534b64584a38ec
pvn-mn <prenavan.m@gmail.com>
2025-06-07 16:38:48 +0530

    Make payment summary interactive
    in orderSummary.js

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -7,10 +7,12 @@ import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 // hello(); - execution error explained in checkout.html
 
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import {renderPaymentSummary} from './paymentSummary.js';
 
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+
+// const today = dayjs();
+// const deliveryDate = today.add(7, 'days');
+// console.log(deliveryDate.format('dddd, MMMM D'));
 
 
 export function renderOrderSummary(){
@@ -125,6 +127,8 @@ export function renderOrderSummary(){
             const container = document.querySelector(`.js-cart-item-container-${productId}`);
             // console.log(container);
             container.remove();
+
+            renderPaymentSummary();
         });
     });
 
@@ -134,6 +138,7 @@ export function renderOrderSummary(){
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
     });
     });
 }
